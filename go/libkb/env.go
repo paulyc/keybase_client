safino9/client

CLI- mountdir config in go

go/libkb/env.go
@@ -97,6 +97,7 @@ func (n NullConfiguration) GetGregorPingTimeout() (time.Duration, bool)     { re
 func (n NullConfiguration) GetChatDelivererInterval() (time.Duration, bool) { return 0, false }
 func (n NullConfiguration) GetGregorDisabled() (bool, bool)                 { return false, false }
 func (n NullConfiguration) GetMountDir() string                             { return "" }
+func (n NullConfiguration) GetMountDirDefault() string                      { return "" }
 func (n NullConfiguration) GetBGIdentifierDisabled() (bool, bool)           { return false, false }
 func (n NullConfiguration) GetFeatureFlags() (FeatureFlags, error)          { return FeatureFlags{}, nil }
 func (n NullConfiguration) GetAppType() AppType                             { return NoAppType }
@@ -272,41 +273,52 @@ func (e *Env) GetUpdaterConfig() UpdaterConfigReader {
 	return e.updaterConfig
 }
 
+func (e *Env) GetOldMountDirDefault() string {
+	switch RuntimeGroup() {
+	case keybase1.RuntimeGroup_UNIXLIKE:
+		return filepath.Join(e.GetDataDir(), "fs")
+	default:
+		return e.GetMountDirDefault()
+	}
+}
+
+func (e *Env) GetMountDirDefault() string {
+	switch RuntimeGroup() {
+	case keybase1.RuntimeGroup_DARWINLIKE:
+		volumes := "/Volumes"
+		user, err := user.Current()
+		if err != nil {
+			panic(fmt.Sprintf("Couldn't get current user: %+v", err))
+		}
+		var runmodeName string
+		switch e.GetRunMode() {
+		case DevelRunMode:
+			runmodeName = "KeybaseDevel"
+		case StagingRunMode:
+			runmodeName = "KeybaseStaging"
+		case ProductionRunMode:
+			runmodeName = "Keybase"
+		default:
+			panic("Invalid run mode")
+		}
+		return filepath.Join(volumes, fmt.Sprintf(
+			"%s (%s)", runmodeName, user.Username))
+	case keybase1.RuntimeGroup_UNIXLIKE:
+		return filepath.Join(e.GetRuntimeDir(), "kbfs")
+	// kbfsdokan depends on an empty default
+	case keybase1.RuntimeGroup_WINDOWSLIKE:
+		return ""
+	default:
+		return filepath.Join(e.GetRuntimeDir(), "kbfs")
+	}
+}
+
 func (e *Env) GetMountDir() (string, error) {
 	return e.GetString(
 		func() string { return e.cmd.GetMountDir() },
 		func() string { return os.Getenv("KEYBASE_MOUNTDIR") },
 		func() string { return e.GetConfig().GetMountDir() },
-		func() string {
-			switch runtime.GOOS {
-			case "darwin":
-				volumes := "/Volumes"
-				user, err := user.Current()
-				if err != nil {
-					panic(fmt.Sprintf("Couldn't get current user: %+v", err))
-				}
-				var runmodeName string
-				switch e.GetRunMode() {
-				case DevelRunMode:
-					runmodeName = "KeybaseDevel"
-				case StagingRunMode:
-					runmodeName = "KeybaseStaging"
-				case ProductionRunMode:
-					runmodeName = "Keybase"
-				default:
-					panic("Invalid run mode")
-				}
-				return filepath.Join(volumes, fmt.Sprintf(
-					"%s (%s)", runmodeName, user.Username))
-			case "linux":
-				return filepath.Join(e.GetRuntimeDir(), "kbfs")
-			// kbfsdokan depends on an empty default
-			case "windows":
-				return ""
-			default:
-				return filepath.Join(e.GetRuntimeDir(), "kbfs")
-			}
-		},
+		func() string { return e.GetMountDirDefault() },
 	), nil
 }
 

go/libkb/util.go
@@ -968,3 +968,16 @@ func Once(f func()) func() {
 		once.Do(f)
 	}
 }
+
+func RuntimeGroup() keybase1.RuntimeGroup {
+	switch runtime.GOOS {
+	case "linux", "dragonfly", "freebsd", "netbsd", "openbsd":
+		return keybase1.RuntimeGroup_UNIXLIKE
+	case "darwin":
+		return keybase1.RuntimeGroup_DARWINLIKE
+	case "windows":
+		return keybase1.RuntimeGroup_WINDOWSLIKE
+	default:
+		return keybase1.RuntimeGroup_UNKNOWN
+	}
+}

go/protocol/keybase1/os.go
@@ -1,0 +1,54 @@
+// Auto-generated by avdl-compiler v1.3.29 (https://github.com/keybase/node-avdl-compiler)
+//   Input file: avdl/keybase1/os.avdl
+
+package keybase1
+
+import (
+	"github.com/keybase/go-framed-msgpack-rpc/rpc"
+)
+
+type RuntimeGroup int
+
+const (
+	RuntimeGroup_UNKNOWN     RuntimeGroup = 0
+	RuntimeGroup_UNIXLIKE    RuntimeGroup = 1
+	RuntimeGroup_DARWINLIKE  RuntimeGroup = 2
+	RuntimeGroup_WINDOWSLIKE RuntimeGroup = 3
+)
+
+func (o RuntimeGroup) DeepCopy() RuntimeGroup { return o }
+
+var RuntimeGroupMap = map[string]RuntimeGroup{
+	"UNKNOWN":     0,
+	"UNIXLIKE":    1,
+	"DARWINLIKE":  2,
+	"WINDOWSLIKE": 3,
+}
+
+var RuntimeGroupRevMap = map[RuntimeGroup]string{
+	0: "UNKNOWN",
+	1: "UNIXLIKE",
+	2: "DARWINLIKE",
+	3: "WINDOWSLIKE",
+}
+
+func (e RuntimeGroup) String() string {
+	if v, ok := RuntimeGroupRevMap[e]; ok {
+		return v
+	}
+	return ""
+}
+
+type RuntimeInterface interface {
+}
+
+func RuntimeProtocol(i RuntimeInterface) rpc.Protocol {
+	return rpc.Protocol{
+		Name:    "keybase.1.runtime",
+		Methods: map[string]rpc.ServeHandlerDescription{},
+	}
+}
+
+type RuntimeClient struct {
+	Cli rpc.GenericClient
+}

paulyc/keybase_client

go/libkb/env.go
@@ -97,6 +97,7 @@ func (n NullConfiguration) GetGregorPingTimeout() (time.Duration, bool)     { re
 func (n NullConfiguration) GetChatDelivererInterval() (time.Duration, bool) { return 0, false }
 func (n NullConfiguration) GetGregorDisabled() (bool, bool)                 { return false, false }
 func (n NullConfiguration) GetMountDir() string                             { return "" }
+func (n NullConfiguration) GetMountDirDefault() string                      { return "" }
 func (n NullConfiguration) GetBGIdentifierDisabled() (bool, bool)           { return false, false }
 func (n NullConfiguration) GetFeatureFlags() (FeatureFlags, error)          { return FeatureFlags{}, nil }
 func (n NullConfiguration) GetAppType() AppType                             { return NoAppType }
@@ -273,41 +274,52 @@ func (e *Env) GetUpdaterConfig() UpdaterConfigReader {
 	return e.updaterConfig
 }
 
+func (e *Env) GetOldMountDirDefault() string {
+	switch RuntimeGroup() {
+	case keybase1.RuntimeGroup_LINUXLIKE:
+		return filepath.Join(e.GetDataDir(), "fs")
+	default:
+		return e.GetMountDirDefault()
+	}
+}
+
+func (e *Env) GetMountDirDefault() string {
+	switch RuntimeGroup() {
+	case keybase1.RuntimeGroup_DARWINLIKE:
+		volumes := "/Volumes"
+		user, err := user.Current()
+		if err != nil {
+			panic(fmt.Sprintf("Couldn't get current user: %+v", err))
+		}
+		var runmodeName string
+		switch e.GetRunMode() {
+		case DevelRunMode:
+			runmodeName = "KeybaseDevel"
+		case StagingRunMode:
+			runmodeName = "KeybaseStaging"
+		case ProductionRunMode:
+			runmodeName = "Keybase"
+		default:
+			panic("Invalid run mode")
+		}
+		return filepath.Join(volumes, fmt.Sprintf(
+			"%s (%s)", runmodeName, user.Username))
+	case keybase1.RuntimeGroup_LINUXLIKE:
+		return filepath.Join(e.GetRuntimeDir(), "kbfs")
+	// kbfsdokan depends on an empty default
+	case keybase1.RuntimeGroup_WINDOWSLIKE:
+		return ""
+	default:
+		return filepath.Join(e.GetRuntimeDir(), "kbfs")
+	}
+}
+
 func (e *Env) GetMountDir() (string, error) {
 	return e.GetString(
 		func() string { return e.cmd.GetMountDir() },
 		func() string { return os.Getenv("KEYBASE_MOUNTDIR") },
 		func() string { return e.GetConfig().GetMountDir() },
-		func() string {
-			switch runtime.GOOS {
-			case "darwin":
-				volumes := "/Volumes"
-				user, err := user.Current()
-				if err != nil {
-					panic(fmt.Sprintf("Couldn't get current user: %+v", err))
-				}
-				var runmodeName string
-				switch e.GetRunMode() {
-				case DevelRunMode:
-					runmodeName = "KeybaseDevel"
-				case StagingRunMode:
-					runmodeName = "KeybaseStaging"
-				case ProductionRunMode:
-					runmodeName = "Keybase"
-				default:
-					panic("Invalid run mode")
-				}
-				return filepath.Join(volumes, fmt.Sprintf(
-					"%s (%s)", runmodeName, user.Username))
-			case "linux":
-				return filepath.Join(e.GetRuntimeDir(), "kbfs")
-			// kbfsdokan depends on an empty default
-			case "windows":
-				return ""
-			default:
-				return filepath.Join(e.GetRuntimeDir(), "kbfs")
-			}
-		},
+		func() string { return e.GetMountDirDefault() },
 	), nil
 }
 

go/libkb/util.go
@@ -968,3 +968,16 @@ func Once(f func()) func() {
 		once.Do(f)
 	}
 }
+
+func RuntimeGroup() keybase1.RuntimeGroup {
+	switch runtime.GOOS {
+	case "linux", "dragonfly", "freebsd", "netbsd", "openbsd":
+		return keybase1.RuntimeGroup_LINUXLIKE
+	case "darwin":
+		return keybase1.RuntimeGroup_DARWINLIKE
+	case "windows":
+		return keybase1.RuntimeGroup_WINDOWSLIKE
+	default:
+		return keybase1.RuntimeGroup_UNKNOWN
+	}
+}

go/protocol/keybase1/os.go
@@ -1,0 +1,54 @@
+// Auto-generated by avdl-compiler v1.3.29 (https://github.com/keybase/node-avdl-compiler)
+//   Input file: avdl/keybase1/os.avdl
+
+package keybase1
+
+import (
+	"github.com/keybase/go-framed-msgpack-rpc/rpc"
+)
+
+type RuntimeGroup int
+
+const (
+	RuntimeGroup_UNKNOWN     RuntimeGroup = 0
+	RuntimeGroup_LINUXLIKE   RuntimeGroup = 1
+	RuntimeGroup_DARWINLIKE  RuntimeGroup = 2
+	RuntimeGroup_WINDOWSLIKE RuntimeGroup = 3
+)
+
+func (o RuntimeGroup) DeepCopy() RuntimeGroup { return o }
+
+var RuntimeGroupMap = map[string]RuntimeGroup{
+	"UNKNOWN":     0,
+	"LINUXLIKE":   1,
+	"DARWINLIKE":  2,
+	"WINDOWSLIKE": 3,
+}
+
+var RuntimeGroupRevMap = map[RuntimeGroup]string{
+	0: "UNKNOWN",
+	1: "LINUXLIKE",
+	2: "DARWINLIKE",
+	3: "WINDOWSLIKE",
+}
+
+func (e RuntimeGroup) String() string {
+	if v, ok := RuntimeGroupRevMap[e]; ok {
+		return v
+	}
+	return ""
+}
+
+type RuntimeInterface interface {
+}
+
+func RuntimeProtocol(i RuntimeInterface) rpc.Protocol {
+	return rpc.Protocol{
+		Name:    "keybase.1.runtime",
+		Methods: map[string]rpc.ServeHandlerDescription{},
+	}
+}
+
+type RuntimeClient struct {
+	Cli rpc.GenericClient
+}